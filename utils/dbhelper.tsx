fbe3d7c1993424269f616c2b4813ee2819aebc00
Jacky Zhang <jacky.zsn@gmail.com>
2025-10-15 08:48:35 +0800

    Fix backup restore mongodb cloud

--- utils/dbhelper.tsx
+++ utils/dbhelper.tsx
@@ -315,183 +315,183 @@ export async function importFromFile(
     }
 }
 
-export async function syncToCloud(
-    deviceId: string,
-    userAgent: string,
-    callback: (code: string) => void
-): Promise<void> {
-    (await db).transaction((trans: Transaction) => {
-        trans.executeSql(
-            'SELECT id, note_group, note_tag, note_text, delete_key, updt from tbl_notes order by id',
-            [],
-            (tx: Transaction, results: ResultSet) => {
-                let noteList: Array<any> = [];
+// export async function syncToCloud(
+//     deviceId: string,
+//     userAgent: string,
+//     callback: (code: string) => void
+// ): Promise<void> {
+//     (await db).transaction((trans: Transaction) => {
+//         trans.executeSql(
+//             'SELECT id, note_group, note_tag, note_text, delete_key, updt from tbl_notes order by id',
+//             [],
+//             (tx: Transaction, results: ResultSet) => {
+//                 let noteList: Array<any> = [];
 
-                if (results.rows.length > 0) {
-                    for (let i = 0; i < results.rows.length; i++) {
-                        let rec: any = {};
-                        rec.id = results.rows.item(i).id;
-                        rec.note_tag = results.rows.item(i).note_tag;
-                        rec.updt = results.rows.item(i).updt;
-                        rec.note_content = results.rows.item(i).note_text;
-                        rec.note_group = results.rows.item(i).note_group;
-                        rec.delete_key = results.rows.item(i).delete_key;
-                        noteList.push(rec);
-                    }
-                }
+//                 if (results.rows.length > 0) {
+//                     for (let i = 0; i < results.rows.length; i++) {
+//                         let rec: any = {};
+//                         rec.id = results.rows.item(i).id;
+//                         rec.note_tag = results.rows.item(i).note_tag;
+//                         rec.updt = results.rows.item(i).updt;
+//                         rec.note_content = results.rows.item(i).note_text;
+//                         rec.note_group = results.rows.item(i).note_group;
+//                         rec.delete_key = results.rows.item(i).delete_key;
+//                         noteList.push(rec);
+//                     }
+//                 }
 
-                let now = moment();
-                let nowString = now.format('YYYY-MM-DDTHH:mm:ss.SSS');
-                UUIDGenerator.getRandomUUID().then(uid => {
-                    fetch(`${setting.backupURL}${mynote}/items`, {
-                        method: 'POST',
-                        headers: {
-                            Accept: 'application/json',
-                            'Content-Type': 'application/json',
-                            'X-Api-Key': setting.backupAPIKey,
-                        },
-                        body: JSON.stringify({
-                            item: {
-                                uuid: uid,
-                                agent: userAgent,
-                                content: JSON.stringify(noteList),
-                            },
-                        }),
-                    })
-                        .then(_response => {
-                            callback('00');
-                            fetch(`${setting.backupURL}${mynoteIndex}/items`, {
-                                method: 'POST',
-                                headers: {
-                                    Accept: 'application/json',
-                                    'Content-Type': 'application/json',
-                                    'X-Api-Key': setting.backupAPIKey,
-                                },
-                                body: JSON.stringify({
-                                    item: {
-                                        uuid: uid,
-                                        device: deviceId,
-                                        updt: nowString,
-                                    },
-                                }),
-                            });
-                        })
-                        .catch(error => {
-                            console.error(error);
-                            callback('99');
-                        });
-                });
-            }
-        );
-    });
-}
+//                 let now = moment();
+//                 let nowString = now.format('YYYY-MM-DDTHH:mm:ss.SSS');
+//                 UUIDGenerator.getRandomUUID().then(uid => {
+//                     fetch(`${setting.backupURL}${mynote}/items`, {
+//                         method: 'POST',
+//                         headers: {
+//                             Accept: 'application/json',
+//                             'Content-Type': 'application/json',
+//                             'X-Api-Key': setting.backupAPIKey,
+//                         },
+//                         body: JSON.stringify({
+//                             item: {
+//                                 uuid: uid,
+//                                 agent: userAgent,
+//                                 content: JSON.stringify(noteList),
+//                             },
+//                         }),
+//                     })
+//                         .then(_response => {
+//                             callback('00');
+//                             fetch(`${setting.backupURL}${mynoteIndex}/items`, {
+//                                 method: 'POST',
+//                                 headers: {
+//                                     Accept: 'application/json',
+//                                     'Content-Type': 'application/json',
+//                                     'X-Api-Key': setting.backupAPIKey,
+//                                 },
+//                                 body: JSON.stringify({
+//                                     item: {
+//                                         uuid: uid,
+//                                         device: deviceId,
+//                                         updt: nowString,
+//                                     },
+//                                 }),
+//                             });
+//                         })
+//                         .catch(error => {
+//                             console.error(error);
+//                             callback('99');
+//                         });
+//                 });
+//             }
+//         );
+//     });
+// }
 
 
-export function retrieveBackups(
-    callback: (backupList: Array<BackupType>) => void,
-    showError: () => void
-): void {
-    fetch(`${setting.backupURL}${mynoteIndex}/query`, {
-        method: 'POST',
-        headers: {
-            Accept: 'application/json',
-            'Content-Type': 'application/json',
-            'X-Api-Key': setting.backupAPIKey,
-        },
-        body: JSON.stringify({
-            query: [],
-        }),
-    })
-        .then(response => {
-            if (!response.ok) {
-                showError();
-                console.error('HTTP error ' + response.status);
-            }
-            return response.json();
-        })
-        .then(json => {
-            let backupList = json.items.map((item: any) => ({
-                uuid: item.uuid,
-                device: item.device,
-                backupAt: item.updt,
-            }));
+// export function retrieveBackups(
+//     callback: (backupList: Array<BackupType>) => void,
+//     showError: () => void
+// ): void {
+//     fetch(`${setting.backupURL}${mynoteIndex}/query`, {
+//         method: 'POST',
+//         headers: {
+//             Accept: 'application/json',
+//             'Content-Type': 'application/json',
+//             'X-Api-Key': setting.backupAPIKey,
+//         },
+//         body: JSON.stringify({
+//             query: [],
+//         }),
+//     })
+//         .then(response => {
+//             if (!response.ok) {
+//                 showError();
+//                 console.error('HTTP error ' + response.status);
+//             }
+//             return response.json();
+//         })
+//         .then(json => {
+//             let backupList = json.items.map((item: any) => ({
+//                 uuid: item.uuid,
+//                 device: item.device,
+//                 backupAt: item.updt,
+//             }));
 
-            backupList.sort((a: BackupType, b: BackupType) => {
-                if (b.backupAt > a.backupAt) {
-                    return 1;
-                }
-                if (a.backupAt > b.backupAt) {
-                    return -1;
-                }
-                return 0;
-            });
+//             backupList.sort((a: BackupType, b: BackupType) => {
+//                 if (b.backupAt > a.backupAt) {
+//                     return 1;
+//                 }
+//                 if (a.backupAt > b.backupAt) {
+//                     return -1;
+//                 }
+//                 return 0;
+//             });
 
-            callback(backupList);
-        })
-        .catch(error => {
-            console.error(error);
-            showError();
-            callback([]);
-        });
-}
+//             callback(backupList);
+//         })
+//         .catch(error => {
+//             console.error(error);
+//             showError();
+//             callback([]);
+//         });
+// }
 
-export function restoreToDB(
-    uuid: string,
-    callback: (code: string) => void
-): void {
-    fetch(`${setting.backupURL}${mynote}/query`, {
-        method: 'POST',
-        headers: {
-            Accept: 'application/json',
-            'Content-Type': 'application/json',
-            'X-Api-Key': setting.backupAPIKey,
-        },
-        body: JSON.stringify({
-            query: [{ uuid: uuid }],
-        }),
-    })
-        .then(response => {
-            if (!response.ok) {
-                callback('99');
-                console.error('HTTP error ' + response.status);
-            }
-            return response.json();
-        })
-        .then(async json => {
-            let notes = JSON.parse(json.items[0].content);
+// export function restoreToDB(
+//     uuid: string,
+//     callback: (code: string) => void
+// ): void {
+//     fetch(`${setting.backupURL}${mynote}/query`, {
+//         method: 'POST',
+//         headers: {
+//             Accept: 'application/json',
+//             'Content-Type': 'application/json',
+//             'X-Api-Key': setting.backupAPIKey,
+//         },
+//         body: JSON.stringify({
+//             query: [{ uuid: uuid }],
+//         }),
+//     })
+//         .then(response => {
+//             if (!response.ok) {
+//                 callback('99');
+//                 console.error('HTTP error ' + response.status);
+//             }
+//             return response.json();
+//         })
+//         .then(async json => {
+//             let notes = JSON.parse(json.items[0].content);
 
-            (await db).transaction((trans: Transaction) => {
-                trans.executeSql('DELETE from tbl_notes', []);
-                let symbols = '';
-                let vals: Array<string | number> = [];
-                for (let i = 0; i < notes.length; i++) {
-                    let noteTag = notes[i].note_tag;
-                    let noteContent = notes[i].note_content;
-                    let noteGroup = notes[i].note_group;
-                    let deleteKey = notes[i].delete_key;
-                    let updt = notes[i].updt;
-                    vals.push(noteGroup, noteTag, updt, noteContent, deleteKey);
-                    symbols += i === 0 ? '(?,?,?,?,?)' : ',(?,?,?,?,?)';
-                }
-                trans.executeSql(
-                    'INSERT into tbl_notes (note_group, note_tag, updt, note_text, delete_key) values ' + symbols,
-                    [...vals],
-                    (tx: Transaction, results: ResultSet) => {
-                        if (results.rowsAffected === 0) {
-                            callback('99');
-                            console.log('Failed to insert');
-                        } else {
-                            callback('00');
-                        }
-                    }
-                );
-            });
-        })
-        .catch(error => {
-            console.error(error);
-            callback('99');
-        });
-}
+//             (await db).transaction((trans: Transaction) => {
+//                 trans.executeSql('DELETE from tbl_notes', []);
+//                 let symbols = '';
+//                 let vals: Array<string | number> = [];
+//                 for (let i = 0; i < notes.length; i++) {
+//                     let noteTag = notes[i].note_tag;
+//                     let noteContent = notes[i].note_content;
+//                     let noteGroup = notes[i].note_group;
+//                     let deleteKey = notes[i].delete_key;
+//                     let updt = notes[i].updt;
+//                     vals.push(noteGroup, noteTag, updt, noteContent, deleteKey);
+//                     symbols += i === 0 ? '(?,?,?,?,?)' : ',(?,?,?,?,?)';
+//                 }
+//                 trans.executeSql(
+//                     'INSERT into tbl_notes (note_group, note_tag, updt, note_text, delete_key) values ' + symbols,
+//                     [...vals],
+//                     (tx: Transaction, results: ResultSet) => {
+//                         if (results.rowsAffected === 0) {
+//                             callback('99');
+//                             console.log('Failed to insert');
+//                         } else {
+//                             callback('00');
+//                         }
+//                     }
+//                 );
+//             });
+//         })
+//         .catch(error => {
+//             console.error(error);
+//             callback('99');
+//         });
+// }
 
 export async function syncToMongo(
     deviceId: string,
@@ -521,25 +521,18 @@ export async function syncToMongo(
                 let now = moment();
                 let nowString = now.format('YYYY-MM-DDTHH:mm:ss.SSS');
                 UUIDGenerator.getRandomUUID().then(uid => {
-                    fetch(`${setting.mongodb.backupURL}/insertOne`, {
+                    fetch(`${setting.mongodb.backupURL}/api/notes/`, {
                         method: 'POST',
                         headers: {
                             Accept: 'application/json',
-                            'Content-Type': 'application/json',
-                            'apiKey': setting.mongodb.apiKey,
+                            'Content-Type': 'application/json'
                         },
                         body: JSON.stringify({
-                            dataSource: setting.mongodb.dataSource,
-                            database: setting.mongodb.database,
-                            collection: setting.mongodb.collection,
-                            document: {
-                                uuid: uid,
-                                agent: userAgent,
-                                content: JSON.stringify(noteList),
-                                device: deviceId,
-                                updt: nowString,
-                            },
-                        }),
+                            uuid: uid,
+                            agent: userAgent,
+                            content: JSON.stringify(noteList),
+                            device: deviceId
+                        })
                     })
                         .then(_response => {
                             callback('00');
@@ -558,22 +551,12 @@ export function retrieveBackupsMongo(
     callback: (backupList: Array<BackupType>) => void,
     showError: () => void
 ): void {
-    fetch(`${setting.mongodb.backupURL}/find`, {
-        method: 'POST',
+    fetch(`${setting.mongodb.backupURL}/api/notes/`, {
+        method: 'GET',
         headers: {
             Accept: 'application/json',
             'Content-Type': 'application/json',
-            'apiKey': setting.mongodb.apiKey,
-        },
-        body: JSON.stringify({
-            dataSource: setting.mongodb.dataSource,
-            database: setting.mongodb.database,
-            collection: setting.mongodb.collection,
-            projection: { uuid: 1, device: 1, updt: 1 },
-            sort: {
-                updt: -1,
-            },
-        }),
+        }
     })
         .then(response => {
             if (!response.ok) {
@@ -583,7 +566,7 @@ export function retrieveBackupsMongo(
             return response.json();
         })
         .then(json => {
-            let backupList = json.documents.map((item: any) => ({
+            let backupList = json.map((item: any) => ({
                 uuid: item.uuid,
                 device: item.device,
                 backupAt: item.updt,
@@ -612,21 +595,13 @@ export function restoreToDBMongo(
     uuid: string,
     callback: (code: string) => void
 ): void {
-    fetch(`${setting.mongodb.backupURL}/findOne`, {
-        method: 'POST',
+    console.log(uuid);
+    fetch(`${setting.mongodb.backupURL}/api/notes/${uuid}`, {
+        method: 'GET',
         headers: {
             Accept: 'application/json',
-            'Content-Type': 'application/json',
-            'apiKey': setting.mongodb.apiKey,
-        },
-        body: JSON.stringify({
-            dataSource: setting.mongodb.dataSource,
-            database: setting.mongodb.database,
-            collection: setting.mongodb.collection,
-            filter: {
-                uuid,
-            },
-        }),
+            'Content-Type': 'application/json'
+        }
     })
         .then(response => {
             if (!response.ok) {
@@ -636,7 +611,7 @@ export function restoreToDBMongo(
             return response.json();
         })
         .then(async json => {
-            let notes = JSON.parse(json.document.content);
+            let notes = JSON.parse(json.content);
 
             (await db).transaction((trans: Transaction) => {
                 trans.executeSql('DELETE from tbl_notes', []);
