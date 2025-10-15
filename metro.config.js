ed479061d9b940c19e54b6198b15cb7e63031b6b
Jacky Zhang <jacky.zsn@gmail.com>
2025-10-15 17:32:34 +0800

    Fix backup rstore from mongodb atlas

--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,21 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 
-/**
- * Metro configuration
- * https://reactnative.dev/docs/metro
- *
- * @type {import('metro-config').MetroConfig}
- */
-const config = {};
+const config = {
+  transformer: {
+    // Ensure we use terser and do NOT mangle property names
+    minifierPath: require.resolve('metro-minify-terser'),
+    minifierConfig: {
+      keep_classnames: true,
+      keep_fnames: true,
+      mangle: {
+        properties: false, // <-- critical
+      },
+      output: {
+        comments: false,
+        ascii_only: true,
+      },
+    },
+  },
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
